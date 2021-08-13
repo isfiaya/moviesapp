db137f2b9350664a4c61010f60924aabfa41c660
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-08-13 14:46:50 +0200

    dev: Remove react flip

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,9 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-import { forwardRef } from "react";
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div ref={ref} className="p-2 group cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div className="p-2 group cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -28,6 +27,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-})
+}
 
 export default Thumbnail;
