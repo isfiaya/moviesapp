0e478cf700360395d29d38b509108fa1bfb966aa
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-08-13 14:38:29 +0200

    remove react strict mode

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,10 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-
-function Thumbnail({ result }) {
+import { forwardRef } from "react";
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="p-2 group cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div ref={ref} className="p-2 group cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -28,6 +28,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+})
 
 export default Thumbnail;
